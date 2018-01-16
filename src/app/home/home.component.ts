10292ed9ff33ae4ea6826701b0baaa69f144324d
digitopoi <hubo85@gmail.com>
2018-01-15 23:00:38 -0600

    Add cancel register and toggle display

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -15,7 +15,11 @@ export class HomeComponent implements OnInit {
   }
 
   registerToggle() {
-    this.registerMode = !this.registerMode;
+    this.registerMode = true;
+  }
+
+  cancelRegisterMode(registerMode: boolean) {
+    this.registerMode = registerMode;
   }
 
 }
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-register',
@@ -6,8 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-
   model: any = {};
+  @Output() cancelRegister = new EventEmitter();
 
   constructor() { }
 
@@ -19,6 +19,7 @@ export class RegisterComponent implements OnInit {
   }
 
   cancel() {
+    this.cancelRegister.emit(false);
     console.log('cancelled');
   }
 
