20736eba2ab769f0b29dc704ced5b5f77f9e7c6a
digitopoi <hubo85@gmail.com>
2018-01-30 13:56:05 -0600

    Add routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,22 @@ import { HttpModule } from '@angular/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BsDropdownModule } from 'ngx-bootstrap';
+import { RouterModule } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
 import { ValueComponent } from './value/value.component';
 import { NavComponent } from './nav/nav.component';
-
-import { AuthService } from './_services/auth.service';
-import { AlertifyService } from './_services/alertify.service';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { MemberListComponent } from './member-list/member-list.component';
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
+
+import { AuthService } from './_services/auth.service';
+import { AlertifyService } from './_services/alertify.service';
+import { appRoutes } from './routes';
+
 
 
 @NgModule({
@@ -33,7 +36,8 @@ import { MessagesComponent } from './messages/messages.component';
     BrowserModule,
     HttpModule,
     FormsModule,
-    BsDropdownModule.forRoot()
+    BsDropdownModule.forRoot(),
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [
     AuthService,
